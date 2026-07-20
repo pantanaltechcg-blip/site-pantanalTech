72765f7efc6c37b7939c1bef7b79b7397178a12b
Carlos Eduardo Furlan <carloseduardofurlan78@gmail.com>
2026-07-20 12:22:08 -0400

    refactor(header): ajuste dos links de navegação

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,10 +5,10 @@ class Header extends HTMLElement {
                 <img src="../../assets/logo-pantanaltech.jpeg" class="logo" alt="Logo">
 
                 <nav class="menu">
-                    <a href="#" class="menu-link">Home</a>
-                    <a href="#" class="menu-link">Quem Somos</a>
+                    <a href="index.html" class="menu-link">Home</a>
+                    <a href="quemsomos.html" class="menu-link">Quem Somos</a>
                     <a href="#" class="menu-link">Serviços</a>
-                    <a href="#" class="menu-link">Projetos</a>
+                    <a href="projetos.html" class="menu-link">Projetos</a>
 
                     <button class="menu-btn">Fale Conosco</button>
                     </nav>
